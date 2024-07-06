a3ab40810abc78c84cdb29c189486aae7a5e1351
20106050083 <agungrashif009@gmail.com>
2024-07-06 13:41:13 +0700

    server running...

--- index.ts
+++ index.ts
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running...`);
 });
 
 export default app;
